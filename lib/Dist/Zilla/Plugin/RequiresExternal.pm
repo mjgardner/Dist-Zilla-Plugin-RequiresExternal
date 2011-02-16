c24cb725022cd5d99ff2e62d19cf6a49f68da497
gardnerm <gardnerm@gsicommerce.com>
2011-02-16 17:36:06 -0500

    add the actual test script template and the code to add it, as well as simple lives_ok tests

--- lib/Dist/Zilla/Plugin/RequiresExternal.pm
+++ lib/Dist/Zilla/Plugin/RequiresExternal.pm
@@ -5,10 +5,12 @@ package Dist::Zilla::Plugin::RequiresExternal;
 use Moose;
 use MooseX::Types::Moose qw(ArrayRef Str);
 use MooseX::Has::Sugar;
+use Dist::Zilla::File::InMemory;
 use namespace::autoclean;
 with qw(
     Dist::Zilla::Role::Plugin
     Dist::Zilla::Role::FileGatherer
+    Dist::Zilla::Role::TextTemplate
 );
 
 =begin Pod::Coverage
@@ -36,6 +38,8 @@ has _requires => ( ro, lazy, auto_deref,
 
 =method gather_files
 
+Adds a F<t/requires_external.t> test script to the distribution that checks
+if each L</requires> item is executable.
 Required by
 L<Dist::Zilla::Role::FileGatherer|Dist::Zilla::Role::FileGatherer>.
 
@@ -43,6 +47,15 @@ L<Dist::Zilla::Role::FileGatherer|Dist::Zilla::Role::FileGatherer>.
 
 sub gather_files {
     my $self = shift;
+    $self->add_file(
+        Dist::Zilla::File::InMemory->new(
+            name    => 't/requires_external.t',
+            content => $self->fill_in_string(
+                <<'END_TEMPLATE', { requires => [ $self->_requires ] } ) ) );
+#!perl
+use Test::More tests => {{ scalar @requires }};
+ok(-x $_, "$_ is executable") for qw( {{ "@requires" }} );
+END_TEMPLATE
     return;
 }
 
--- t/test.t
+++ t/test.t
@@ -2,23 +2,26 @@
 
 use Config;
 use English '-no_match_vars';
-use Test::More;
+use Test::Most tests => 2;
 use Test::DZil;
-use File::Temp;
 
 my $perl = $EXECUTABLE_NAME;
 if ( $OSNAME ne 'VMS' ) {
     $perl .= $Config{_exe} unless $perl =~ m/$Config{_exe}$/i;
 }
-
-my $dist_root = File::Temp->newdir();
-my $tzil      = Builder->from_config(
-    { dist_root => "$dist_root" },
+my $tzil = Builder->from_config(
+    { dist_root => 1 },
     {   add_files => {
-            'source/dist.ini' =>
-                simple_ini( [ RequiresExternal => { requires => $perl } ] )
-        }
+            'source/dist.ini' => simple_ini(
+                ['@Basic'], [ RequiresExternal => { requires => $perl } ],
+            ),
+            'source/lib/DZT/Sample.pm' => <<END_SAMPLE_PM,
+package DZT::Sample;
+# ABSTRACT: Sample package
+1;
+END_SAMPLE_PM
+        },
     },
 );
-pass();
-done_testing();
+lives_ok( sub { $tzil->build() }, 'build' );
+lives_ok( sub { $tzil->run_tests_in( $tzil->built_in ) }, 'test' );
